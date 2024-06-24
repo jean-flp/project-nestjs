cd598fb93809401a9f37e893c7d3a2ac7ea8d5aa
jean-flp <jean.felipe.ferreira.freitas@gmail.com>
2024-06-24 18:48:55 -0300

    UserEntity - created// constructor unit test + UserDomain refactoring

---
+++ src/users/domain/entities/user.entity.ts
@@ -0,0 +1,14 @@
+//props q um usuario ira possuir
+export type UserProps = {
+    name : string
+    email: string
+    password: string
+    createdAt?: Date
+}
+
+
+export class UserEntity {
+    constructor(public readonly props:UserProps){
+        this.props.createdAt = this.props.createdAt ?? new Date();
+    }
+}
